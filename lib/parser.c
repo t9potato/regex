9a8869e18e4807f8cd05128563cac073577c7a3e
CARH <hcarpenter@cnsstu.catholic.edu.au>
2023-08-26 06:31:51 +1000

    Initial?

--- lib/parser.c
+++ lib/parser.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 typedef enum {
     ONE,
+    OPTIONAL,
     ONE_OR_MORE,
     ZERO_OR_MORE,
 } match;
@@ -11,12 +14,78 @@ typedef struct TOKEN{
     char symbol;
 } token;
 
-typedef union value value;
-union value{
-    value* v;
+union item{
+    union item* v;
     token t;
 };
 
-char* parse(char* input, int input_lent) {
-    return NULL;
+typedef struct {
+    char key;
+    union item value;
+} value;
+
+value* parse(char* in, int in_len, int* out_len) {
+    *out_len = 0;
+    int max_len = 10;
+    bool escaped = false;
+    value* out = malloc(max_len*sizeof(value));
+    for (int i; i<in_len; i++) {
+        if (escaped) {
+            out[*out_len].key = 'v';
+            out[*out_len].value.t.state = ONE;
+            out[*out_len].value.t.symbol = in[i];
+            (*out_len)++;
+            escaped = false;
+        } else {
+            switch (in[i]) {
+                case '\\':
+                    escaped = true;
+                    break;
+                case '(':
+                    break;
+                case ')':
+                    break;
+                case '+':
+                    if (out[*out_len].value.t.state != ONE) {
+                        fprintf(stderr, "Invalid syntax");
+                        return NULL;
+                    }
+                    out[*out_len].value.t.state = ONE_OR_MORE;
+                    break;
+                case '*':
+                    if (out[*out_len].value.t.state != ONE) {
+                        fprintf(stderr, "Invalid syntax");
+                        return NULL;
+                    }
+                    out[*out_len].value.t.state = ZERO_OR_MORE;
+                    break;
+                case '?':
+                    if (out[*out_len].value.t.state != ONE) {
+                        fprintf(stderr, "Invalid syntax");
+                        return NULL;
+                    }
+                    out[*out_len].value.t.state = OPTIONAL;
+                    break;
+                case '.':
+                    break;
+                default:
+                    out[*out_len].key = 'v';
+                    out[*out_len].value.t.state = ONE;
+                    out[*out_len].value.t.symbol = in[i];
+                    (*out_len)++;
+                    break;
+            }
+        }
+        if ((*out_len) == max_len) {
+            max_len *= 2;
+            value* tmp = realloc(out, max_len*sizeof(value));
+            if (tmp == NULL) {
+                free(out);
+                fprintf(stderr, "Failed to allocate memory: aborting");
+                return NULL;
+            }
+            out = tmp;
+        }
+    }
+    return out;
 }
